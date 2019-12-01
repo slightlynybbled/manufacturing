de8dc2534a5bfdbd0fb528ddb9e402de35c3abbf
Jason R. Jones <slightlynybbled@gmail.com>
2019-11-30 19:36:56 -0500

    Better control chart with more samples.

--- examples/control_chart.py
+++ examples/control_chart.py
@@ -3,6 +3,6 @@ from manufacturing import import_excel, show_control_chart, show_cpk
 
 logging.basicConfig(level=logging.INFO)
 
-data = import_excel('example_data.xlsx', columnname='value (lsl=-2.5 usl=2.5)', skiprows=3)
+data = import_excel('example_data_with_faults.xlsx', columnname='value (lsl=-6.0 usl=6.0)', skiprows=3)
 
 show_control_chart(**data)
