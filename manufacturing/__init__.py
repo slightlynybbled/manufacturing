8664ac5bf91154238a17be82ba7ed92b7f1c98e2
Jason R. Jones <slightlynybbled@gmail.com>
2019-12-01 16:28:41 -0500

    Bumped version with milestone of zone control plots essential complete.

--- manufacturing/__init__.py
+++ manufacturing/__init__.py
@@ -5,4 +5,4 @@ from manufacturing.visual import cpk_plot, control_plot
 __all__ = ['calc_cpk', 'cpk_plot', 'control_plot',
            'import_csv', 'import_excel']
 
-__version__ = '0.5.1'
+__version__ = '0.6.0'
