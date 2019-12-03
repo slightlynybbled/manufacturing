edbc34ee2d6a1e5338c1e21cd5a6f0ffa1ecda3e
Jason R. Jones <slightlynybbled@gmail.com>
2019-12-03 08:07:30 -0500

    Refactoring based on move of example data into its own directory.

--- examples/control_chart.py
+++ examples/control_chart.py
@@ -4,7 +4,7 @@ from manufacturing import import_excel, control_plot
 
 logging.basicConfig(level=logging.INFO)
 
-data = import_excel('example_data_with_faults.xlsx', columnname='value (lcl=-7.4 ucl=7.4)', skiprows=3)
+data = import_excel('data/example_data_with_faults.xlsx', columnname='value (lcl=-7.4 ucl=7.4)', skiprows=3)
 
 control_plot(**data)
 plt.show()
--- examples/import_from_csv.py
+++ examples/import_from_csv.py
@@ -1,11 +1,32 @@
 import logging
 from manufacturing import import_csv, calc_ppk, ppk_plot
+import matplotlib.pyplot as plt
 
 logging.basicConfig(level=logging.INFO)
 
-data = import_csv('example_data.csv', columnname='value (lsl=-2.5 usl=2.5)')
+# First, we will pull in the simplest data with no limits contained in the column
+# headers.  The return type will be a pandas.Series, which we store in `data`:
+data = import_csv('data/example_data-no-limits.csv', columnname='value')
 
-cpk = calc_ppk(**data)
-print(f'cpk = {cpk:.3g}')
+# raw calculation of ppk
+ppk = calc_ppk(data, lower_control_limit=-2.5, upper_control_limit=2.5)
+print(f'Ppk = {ppk:.3g}')
 
+# and then plot
+ppk_plot(data, lower_control_limit=-2.5, upper_control_limit=2.5)
+plt.show()
+
+# -----------------------------------------------------------------------------------
+# Next, we will do the exact same operation as above, but with the limits contained
+# within the column headers.  The format is "columnname (lcl=X ucl=Y)".  The return
+# type is a dictionary with the keys "data", "upper_control_limit", and "lower_control_limit".
+# These key names make it somewhat easier to unpack `data` into `calc_ppk` and `ppk_plot`.
+data = import_csv('data/example_data.csv', columnname='value (lcl=-2.5 ucl=2.5)')
+
+# raw calculation of ppk
+ppk = calc_ppk(**data)
+print(f'Ppk = {ppk:.3g}')
+
+# and then plot
 ppk_plot(**data)
+plt.show()
