5627cfaadad44171b4bc60afb26c298df1609fab
Jason R. Jones <slightlynybbled@gmail.com>
2021-12-21 20:40:19 -0500

    Fix spacing, no change to function

--- examples/report_generation.py
+++ examples/report_generation.py
@@ -4,5 +4,7 @@ from manufacturing import generate_production_report
 
 logging.basicConfig(level=logging.INFO)
 
-generate_production_report('data/example_data_with_faults.csv', output_file=Path('test.pdf'))
-generate_production_report('data/example_data_with_faults.csv', output_file=Path('test.html'))
+generate_production_report('data/example_data_with_faults.csv',
+                           output_file=Path('test.pdf'))
+generate_production_report('data/example_data_with_faults.csv',
+                           output_file=Path('test.html'))
