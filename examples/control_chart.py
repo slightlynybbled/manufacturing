f8ba03beb461129ada8761c5d90120a765978f7e
Jason R. Jones <slightlynybbled@gmail.com>
2022-04-25 09:00:45 -0400

    Add title

--- examples/control_chart.py
+++ examples/control_chart.py
@@ -7,12 +7,13 @@ logging.basicConfig(level=logging.INFO)
 data = import_excel('data/example_data_with_faults.xlsx', columnname='value')
 
 # create an I/MR chart using a combination of control_plot and moving_range
-fig, axs = plt.subplots(2, 1, figsize=(8, 6))
+fig, axs = plt.subplots(2, 1, figsize=(8, 6), sharex='all')
 control_plot(**data, ax=axs[0])
 moving_range(**data, ax=axs[1])
 
 axs[0].set_title('Individual')
 axs[1].set_title('Moving Range')
+fig.suptitle('I/MR Chart')
 
 fig.tight_layout()
 plt.show()
