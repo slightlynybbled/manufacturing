0d4aff2f52d8e6dbb62325008fc62e80ea388b47
Jason R. Jones <slightlynybbled@gmail.com>
2022-04-29 15:16:55 -0400

    Add long file chart example

---
+++ examples/long_file_chart.py
@@ -0,0 +1,15 @@
+"""
+Manual recreation of an I-MR control chart.
+"""
+import logging
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from manufacturing import control_chart
+
+logging.basicConfig(level=logging.INFO)
+
+df = pd.read_csv('data/position-data.txt')
+control_chart(df[' Position Error'], parameter_name='Position Error')
+
+plt.show()
