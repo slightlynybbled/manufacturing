909b999cd40a1394cb03d10eee2b4041af5cb5d0
Jason R. Jones <slightlynybbled@gmail.com>
2022-05-04 21:55:42 -0400

    Organizing tests better

--- test/test_visual.py
+++ test/test_visual.py
@@ -31,36 +31,45 @@ def test_data_type(plot_dir):
     mn.x_mr_chart(data)
 
 
-def test_sizes(plot_dir):
-    path = plot_dir
+def test_x_mr_chart(plot_dir):
+    path = plot_dir / 'x_mr'
+    path.mkdir(exist_ok=True)
 
     # test different sizes to ensure that they get plotted appropriately
     data = np.random.normal(loc=10, scale=1.0, size=2)
     fig = mn.x_mr_chart(data)
     fig.savefig(path / f'test_sizes_{len(data)}.png')
 
-    with pytest.raises(ValueError):
-        mn.xbar_r_chart(data)
-    with pytest.raises(ValueError):
-        mn.xbar_s_chart(data)
-    fig = mn.control_chart(data)
-    fig.savefig(path / f'test_sizes_{len(data)}.png')
-
     for point_sizes in [30, 60, 120, 200]:
-        for size in [29, 35, 59, 60, 61, 119, 120, 121, 599, 600,
-                     601, 1799, 1800, 1801, 2000, 2800]:
+        for size in [120, 600, 1800, 1801, 2000, 2800, 4000]:
             data = np.random.normal(loc=10, scale=1.0, size=size)
             fig = mn.x_mr_chart(data, max_points=point_sizes)
             fig.savefig(path / f'test_sizes_xmr_{point_sizes}_{len(data)}.png')
 
+            plt.cla()
+            plt.close('all')
+
+    plt.close('all')
+
+
+def test_xbar_r_chart(plot_dir):
+    path = plot_dir / 'xbar_r'
+    path.mkdir(exist_ok=True)
+
+    # test different sizes to ensure that they get plotted appropriately
+    data = np.random.normal(loc=10, scale=1.0, size=2)
+
+    with pytest.raises(ValueError):
+        mn.xbar_r_chart(data)
+    fig = mn.control_chart(data)
+    fig.savefig(path / f'test_sizes_{len(data)}.png')
+
+    for point_sizes in [30, 60, 120, 200]:
+        for size in [120, 600, 1800, 1801, 2000, 2800, 4000]:
+            data = np.random.normal(loc=10, scale=1.0, size=size)
+
             fig = mn.xbar_r_chart(data, max_points=point_sizes)
             fig.savefig(path / f'test_sizes_xbarr_{point_sizes}_{len(data)}.png')
-
-            fig = mn.xbar_s_chart(data, max_points=point_sizes)
-            fig.savefig(path / f'test_sizes_xbars_{point_sizes}_{len(data)}.png')
-
-            fig = mn.control_chart(data, max_points=point_sizes)
-            fig.savefig(path / f'test_sizes_cc_{point_sizes}_{len(data)}.png')
 
             plt.cla()
             plt.close('all')
@@ -71,6 +80,32 @@ def test_sizes(plot_dir):
     with pytest.raises(ValueError):
         mn.xbar_r_chart(data, subgroup_size=12)
 
+    plt.close('all')
+
+
+def test_xbar_s_chart(plot_dir):
+    path = plot_dir / 'xbar_s'
+    path.mkdir(exist_ok=True)
+
+    # test different sizes to ensure that they get plotted appropriately
+    data = np.random.normal(loc=10, scale=1.0, size=2)
+
+    with pytest.raises(ValueError):
+        mn.xbar_s_chart(data)
+    fig = mn.control_chart(data)
+    fig.savefig(path / f'test_sizes_{len(data)}.png')
+
+    for point_sizes in [30, 60, 120, 200]:
+        for size in [120, 600, 1800, 1801, 2000, 2800, 4000]:
+            data = np.random.normal(loc=10, scale=1.0, size=size)
+
+            fig = mn.xbar_s_chart(data, max_points=point_sizes)
+            fig.savefig(path / f'test_sizes_xbars_{point_sizes}_{len(data)}.png')
+
+            plt.cla()
+            plt.close('all')
+
+    # modify subgroup sizes to raise value error when improperly specified
     with pytest.raises(ValueError):
         mn.xbar_s_chart(data, subgroup_size=10)
     mn.xbar_s_chart(data, subgroup_size=11)
@@ -81,8 +116,33 @@ def test_sizes(plot_dir):
     plt.close('all')
 
 
+def test_control_chart(plot_dir):
+    path = plot_dir / 'control_chart'
+    path.mkdir(exist_ok=True)
+
+    # test different sizes to ensure that they get plotted appropriately
+    data = np.random.normal(loc=10, scale=1.0, size=2)
+
+    fig = mn.control_chart(data)
+    fig.savefig(path / f'test_sizes_{len(data)}.png')
+
+    for point_sizes in [30, 60, 120, 200]:
+        for size in [29, 35, 59, 60, 61, 119, 120, 121, 599, 600,
+                     601, 1799, 1800, 1801, 2000, 2800]:
+            data = np.random.normal(loc=10, scale=1.0, size=size)
+
+            fig = mn.control_chart(data, max_points=point_sizes)
+            fig.savefig(path / f'test_sizes_cc_{point_sizes}_{len(data)}.png')
+
+            plt.cla()
+            plt.close('all')
+
+    plt.close('all')
+
+
 def test_ppk_plot(plot_dir):
-    path = plot_dir
+    path = plot_dir / 'ppk_plot'
+    path.mkdir(exist_ok=True)
 
     # test different sizes to ensure that they get plotted appropriately
     data = np.random.normal(loc=10, scale=1.0, size=2)
@@ -101,7 +161,8 @@ def test_ppk_plot(plot_dir):
 
 
 def test_cpk_plot(plot_dir):
-    path = plot_dir
+    path = plot_dir / 'cpk_plot'
+    path.mkdir(exist_ok=True)
 
     data = np.random.normal(loc=10, scale=1.0, size=2)
     with pytest.raises(ValueError):
