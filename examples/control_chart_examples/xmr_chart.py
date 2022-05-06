ca5ab5be1ff08e457618e1675d243d40f3ce4ace
Jason R. Jones <slightlynybbled@gmail.com>
2022-05-06 07:05:16 -0400

    Fix data paths

--- examples/control_chart_examples/xmr_chart.py
+++ examples/control_chart_examples/xmr_chart.py
@@ -7,7 +7,7 @@ from manufacturing import import_excel, x_mr_chart
 
 logging.basicConfig(level=logging.INFO)
 
-data = import_excel('data/example_data_with_faults.xlsx', columnname='value')
+data = import_excel('../data/example_data_with_faults.xlsx', columnname='value')
 x_mr_chart(**data, parameter_name='some value',
            highlight_mixture=True,
            highlight_stratification=True,
