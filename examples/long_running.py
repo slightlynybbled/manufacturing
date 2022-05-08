34ea8541f80dfb3254f8005b58501cbdb19d809a
Jason R. Jones <slightlynybbled@gmail.com>
2022-05-07 23:24:54 -0400

    Better example

--- examples/long_running.py
+++ examples/long_running.py
@@ -1,23 +1,37 @@
 from pathlib import Path
+import sys
 
 import numpy as np
-import pandas
 import pandas as pd
 import manufacturing as mn
 import matplotlib.pyplot as plt
 
-# data = np.random.normal(loc=10.0, scale=1.0, size=100000)
-#
-# with open('data/long-running.txt', 'w') as f:
-#     f.write('index,param\n')
-#     for i, point in enumerate(data):
-#         f.write(f'{i},{point}\n')
+response = input('this is a long running process that will create up to 1GB of files on your hard drive and will take a while to run... are you sure that you want to run this?  Y/n')
+if 'n' in response or not response:
+    print('probably for the best\nexiting...')
+    sys.exit()
 
-df = pandas.read_csv('data/long-running.txt')
+data = np.random.normal(loc=10.0, scale=1.0, size=100000)
+
+data_path = Path('data') / 'long-running.txt'
+with data_path.open('w') as f:
+    f.write('index,param\n')
+    for i, point in enumerate(data):
+        f.write(f'{i},{point}\n')
+
+df = pd.read_csv(data_path)
 data = df['param']
 
-for i in range(1800):
+img_path = Path(data_path.parent) / 'long_running'
+img_path.mkdir(exist_ok=True)
+for i in range(2100):
+    print(i)
     new_data = data[0 : i+10]
+
     fig = mn.control_chart(new_data)
-    fig.savefig(f'data/long_running/img_{i}.png')
+    fig.savefig(img_path / f'control_{i}.png')
+
+    fig = mn.x_mr_chart(new_data)
+    fig.savefig(img_path / f'xmr_{i}.png')
+
     plt.close('all')
